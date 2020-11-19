dd7fc1f5877c6b448672ff8575307073b7265fd9
pratushrai0309 <pratushrai0309@gmail.com>
2020-11-19 12:38:19 +0530

    Bug Fixes

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const mongodb = require('mongodb')
+const chalk = require('chalk')
+const figlet = require('figlet')
 
 const command = process.argv[2]
 const bookName = process.argv[3]
 const authorName = process.argv[4]
 
 const {MongoClient} = mongodb
+
+figlet('Book Manager !!', function(err, data) {
+  if (err) {
+      console.log('Something went wrong...');
+      console.dir(err);
+      return;
+  }
+  console.log(data)
+});
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'book-app'
@@ -15,6 +26,16 @@ MongoClient.connect(connectionURL, { useNewUrlParser : true }, (error, client) =
   }
   else{
     const db = client.db(databaseName)
+
+    if(command == 'menu'){
+      console.log(chalk.red('------------------Menu-------------------'));
+      console.log(chalk.green(': add => To a new book'))
+      console.log(chalk.green(': remove => To remove a book'));
+      console.log(chalk.green(': rename => To rename a book'));
+      console.log(chalk.green(': renameAuthor => To rename a author'));
+      console.log(chalk.red('-----------------------------------------'));
+    }
+
     if(command == 'add'){
       db.collection('book').insertOne({
         _bookName : bookName,
@@ -24,7 +45,8 @@ MongoClient.connect(connectionURL, { useNewUrlParser : true }, (error, client) =
           console.log(error);
         }
         else{
-          console.log(books.ops);
+          console.log(books.ops[0]._bookName);
+          console.log(books.ops._authorName);
         }
       })
     }
